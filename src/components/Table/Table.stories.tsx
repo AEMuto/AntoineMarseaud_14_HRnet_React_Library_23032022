ccd01e5e9bdc372672737c23d22869b30415601a
AEMuto <antoine.marseaud@gmail.com>
2022-03-31 16:13:13 +0200

    Get the basic tsx in place for the table component. Added a button component for testing.

--- src/components/Table/Table.stories.tsx
+++ src/components/Table/Table.stories.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import {ComponentStory, ComponentMeta} from "@storybook/react";
-import Table from "./Table";
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Table from './Table';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-    title: "Components/Table",
-    component: Table,
+  title: 'Components/Table',
+  component: Table,
 } as ComponentMeta<typeof Table>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
@@ -14,5 +14,17 @@ const Template: ComponentStory<typeof Table> = (args) => <Table {...args} />;
 export const TableExample = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 TableExample.args = {
-    data: {title: "Hello world!"},
+  data: [{
+    id: "dac01764-2e1c-4d7d-b7b1-5fc455d843d7",
+    firstName: "Casimir",
+    lastName: "Orn",
+    dateOfBirth: "07/27/1981",
+    startDate: "12/17/2013",
+    department: "Engineering",
+    street: "5905 Lemuel Squares",
+    city: "Redondo Beach",
+    state: "Maine",
+    zipCode: "59025"
+  }],
+  title: 'Employees'
 };
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,17 +1,50 @@
-import React from 'react'
+import React from 'react';
 
 type TableProps = {
-    data: {
-        title: string
-    }
-}
+  data: Array<any>;
+  title: string;
+};
 
 const Table = (props: TableProps) => {
-    return (
-        <table>
-            <thead>{props.data.title}</thead>
-        </table>
-    )
-}
+  const { data, title } = props;
+  const keys = Object.keys(data[0]);
+  return (
+    <>
+      <h1>{title}</h1>
+      <table>
+        <thead>
+          <tr>
+            {keys.map((key, index) => {
+              if (key !== 'id') return <th key={`${key}-${index}`}>{key}</th>;
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((obj, index) => {
+            return <Row key={`row-${index}`} data={obj} />;
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+};
 
-export default Table
+export default Table;
+
+const Row = (props: { data: {} }) => {
+  const { data } = props;
+  const entries = Object.entries(data);
+  return (
+    <tr>
+      {entries.map((entry, index) => {
+        if (entry[0] !== 'id' && typeof entry[1] === 'string')
+          return <Cell key={`${entry}-${index}`} entry={entry[1]} />;
+      })}
+    </tr>
+  );
+};
+
+const Cell = (props: { entry: string }) => {
+  const { entry } = props;
+  return <td>{entry}</td>;
+};
