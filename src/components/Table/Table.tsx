22bcbea26c2ec84b35a365a94658161af4289be1
AEMuto <antoine.marseaud@gmail.com>
2022-04-13 19:21:57 +0200

    Refactoring the sort feature. Began style customization.

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -17,6 +17,7 @@ export type dataKey = {
 export type TablePropsOptions = {
   dataKeys?: dataKey[];
   heading?: string;
+  style?: CSSStyleDeclaration;
 };
 
 export type data = Record<string, string | number>;
@@ -26,12 +27,17 @@ export type TableProps = {
   options?: TablePropsOptions;
 };
 
+type sort = (key: string, order: 'asc' | 'desc') => void;
+
 const Table = ({ data = [], options }: TableProps) => {
-  // Default Values
+  /* DEFAULT VALUES ******************************************************************************/
+
   const defaultKeys = Object.keys(data[0]).map((key, index) => {
     return { title: key, value: key, position: index };
   });
+
   const defaultOptions = { heading: 'Data Table', dataKeys: defaultKeys };
+
   const {
     heading = defaultOptions.heading,
     dataKeys = defaultOptions.dataKeys,
@@ -44,13 +50,16 @@ const Table = ({ data = [], options }: TableProps) => {
     { value: 100, label: '100' },
   ];
 
-  // States for the current data, selected pagination and page index
+  /* STATES **************************************************************************************/
+
   const [currentData, setCurrentData] = useState(data);
   const [selectedPagination, setSelectedPagination] = useState(
     paginationOptions[0].value,
   );
   const [pageIndex, setPageIndex] = useState(0);
   const [searchInput, setSearchInput] = useState('');
+  const [sortBy, setSortBy] = useState<string>('');
+  const [sortOrder, setSortOrder] = useState<'asc' | 'desc' | ''>('');
 
   // Building our chunks of data depending on the selected pagination and current data
   const dataChunks = useMemo(
@@ -63,9 +72,43 @@ const Table = ({ data = [], options }: TableProps) => {
 
   // Building the dictionary for the binary search
   const dictionary = useMemo(() => {
-    if(!data.length) return
-    return makeDictionary(data)
-  },[]);
+    if (!data.length) return;
+    return makeDictionary(data);
+  }, []);
+
+  /* HANDLERS ************************************************************************************/
+
+  const handleNextPage = () => {
+    if (pageIndex + 1 >= dataChunks.length) return;
+    setPageIndex(pageIndex + 1);
+  };
+
+  const handlePrevPage = () => {
+    if (pageIndex - 1 < 0) return;
+    setPageIndex(pageIndex - 1);
+  };
+
+  const handlePickPage = _.debounce((e: React.SyntheticEvent<EventTarget>) => {
+    if (!(e.target instanceof HTMLButtonElement)) return;
+    setPageIndex(+e.target.innerHTML - 1);
+  }, 50);
+
+  const handleSort: sort = (key, order) => {
+    const tempData = [...data];
+    sortObjectsArray(tempData, key, order);
+    setCurrentData(() => tempData);
+  };
+
+  const updateSearchInput = _.debounce((e) => {
+    if (!e.target.value.length) {
+      setCurrentData(data);
+      return;
+    }
+    if (e.target.value.length < 3) return;
+    setSearchInput(removeDiacritics(e.target.value.toLowerCase()));
+  }, 500);
+
+  /* EFFECTS *************************************************************************************/
 
   // Modify the displayed data whether the dataChunks changes (meaning a change in the selected pagination)
   // or the pageIndex diminish or augment (meaning the user has clicked on the next|previous button or has
@@ -85,139 +128,110 @@ const Table = ({ data = [], options }: TableProps) => {
     setCurrentData(filteredData);
   }, [searchInput]);
 
-  //console.log('Current data: ', currentData);
+  useEffect(() => {
+    if (!sortBy || !sortOrder) return;
+    console.log('Sort Order : ', sortOrder);
+    console.log('Sorting by : ', sortBy);
+  }, [sortOrder, sortBy]);
 
-  const handleNextPage = () => {
-    //console.log('Next');
-    if (pageIndex + 1 >= dataChunks.length) return;
-    setPageIndex(pageIndex + 1);
-  };
-
-  const handlePrevPage = () => {
-    //console.log('Previous');
-    if (pageIndex - 1 < 0) return;
-    setPageIndex(pageIndex - 1);
-  };
-
-  const handlePickPage = _.debounce((e: React.SyntheticEvent<EventTarget>) => {
-    if (!(e.target instanceof HTMLButtonElement)) return;
-    //console.log(+e.target.innerHTML);
-    setPageIndex(+e.target.innerHTML - 1);
-  }, 50);
-
-  const sortDescending = (e: React.SyntheticEvent<EventTarget>) => {
-    if (!(e.target instanceof HTMLButtonElement)) return;
-    const key = e.target.dataset.key;
-    if (key === undefined) return;
-    //console.log('Sort Descending by', key);
-    const tempData = [...data];
-    sortObjectsArray(tempData, key);
-    setCurrentData(() => tempData);
-  };
-
-  const sortAscending = (e: React.SyntheticEvent<EventTarget>) => {
-    if (!(e.target instanceof HTMLButtonElement)) return;
-    const key = e.target.dataset.key;
-    if (key === undefined) return;
-    //console.log('Sort Ascending', e.target.dataset.key);
-    const tempData = [...data];
-    sortObjectsArray(tempData, key, 'asc');
-    setCurrentData(() => tempData);
-  };
-
-  const updateSearchInput = _.debounce((e) => {
-    if (!e.target.value.length) {
-      setCurrentData(data);
-      return;
-    }
-    if (e.target.value.length < 3) return;
-    setSearchInput(removeDiacritics(e.target.value.toLowerCase()));
-  }, 500);
-
-  //if (!displayedData) return <p>No data to display!</p>
-
+  /* TSX *****************************************************************************************/
   return (
     <DataTable>
       <h1>{heading}</h1>
-      <div className="datatable__pagination-dropdown">
-        <label htmlFor="pagination">
-          Show{'  '}
-          <Dropdown
-            options={paginationOptions}
-            selectedOption={selectedPagination}
-            setSelectedOption={setSelectedPagination}
-          />{' '}
-          entries
-        </label>
+      <div className="datatable__header">
+        <div className="datatable__pagination-dropdown">
+          <label htmlFor="pagination">
+            Show{'  '}
+            <Dropdown
+              options={paginationOptions}
+              selectedOption={selectedPagination}
+              setSelectedOption={setSelectedPagination}
+            />{' '}
+            entries
+          </label>
+        </div>
+        <div className="datatable__search-input">
+          <label htmlFor="search">
+            Search:{'  '}
+            <input type="text" onChange={updateSearchInput} />
+          </label>
+        </div>
       </div>
-      <div className="datatable__search-input">
-        <label htmlFor="search">
-          Search:{'  '}
-          <input type="text" onChange={updateSearchInput} />
-        </label>
-      </div>
-      <table>
-        <thead>
-          <tr>
-            {dataKeys.map((key, index) => {
-              if (key.position !== undefined)
+      <div className="datatable__body">
+        <table>
+          <thead>
+            <tr>
+              {dataKeys.map((key, index) => {
+                const { title, value, position } = key;
+                if (position !== undefined)
+                  return (
+                    <TableHeader
+                      key={`th-${index}`}
+                      title={title}
+                      dataKey={value}
+                      isSorted={sortBy === value}
+                      sortOrder={sortBy === value ? sortOrder : ''}
+                      setSortBy={setSortBy}
+                      setSortOrder={setSortOrder}
+                    />
+                  );
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {displayedData &&
+              displayedData.map((obj, index) => {
                 return (
-                  <th key={`th-${index}`}>
-                    <div className="column-header">
-                      <span className="sort-controller">
-                        <button onClick={sortAscending} data-key={key.value}>
-                          🔺
-                        </button>
-                        <button onClick={sortDescending} data-key={key.value}>
-                          🔻
-                        </button>
-                      </span>
-                      {key.title}
-                    </div>
-                  </th>
+                  <Row
+                    key={`tr-${index}`}
+                    data={obj}
+                    dataKeys={dataKeys}
+                    sortBy={sortBy}
+                    isEven={(index + 1) % 2 === 0}
+                  />
                 );
-            })}
-          </tr>
-        </thead>
-        <tbody>
-          {displayedData && displayedData.map((obj, index) => {
-            return <Row key={`tr-${index}`} data={obj} dataKeys={dataKeys} />;
-          })}
-        </tbody>
-      </table>
-      <div className="datatable__info">
-        <p>
-          Showing{' '}
-          {displayedData && displayedData.length * (pageIndex + 1) - displayedData.length + 1} to{' '}
-          {displayedData && displayedData.length * (pageIndex + 1)} of {currentData.length}{' '}
-          entries
-        </p>
+              })}
+          </tbody>
+        </table>
       </div>
-      <div className="datatable__page-nav">
-        <button
-          onClick={handlePrevPage}
-          disabled={pageIndex === 0}
-          className="btn datatable__prev-page">
-          Previous
-        </button>
-        <span>
-          {dataChunks.map((_, index) => {
-            return (
-              <button
-                key={`page-btn-${index}`}
-                onClick={handlePickPage}
-                className={index === pageIndex ? 'btn current' : 'btn'}>
-                {index + 1}
-              </button>
-            );
-          })}
-        </span>
-        <button
-          onClick={handleNextPage}
-          disabled={pageIndex + 1 === dataChunks.length}
-          className="btn datatable__next-page">
-          Next
-        </button>
+      <div className="datatable__footer">
+        <div className="datatable__info">
+          <p>
+            Showing{' '}
+            {displayedData &&
+              displayedData.length * (pageIndex + 1) -
+                displayedData.length +
+                1}{' '}
+            to {displayedData && displayedData.length * (pageIndex + 1)} of{' '}
+            {currentData.length} entries
+          </p>
+        </div>
+        <div className="datatable__page-nav">
+          <button
+            onClick={handlePrevPage}
+            disabled={pageIndex === 0}
+            className="btn datatable__prev-page">
+            Previous
+          </button>
+          <span>
+            {dataChunks.map((_, index) => {
+              return (
+                <button
+                  key={`page-btn-${index}`}
+                  onClick={handlePickPage}
+                  className={index === pageIndex ? 'btn current' : 'btn'}>
+                  {index + 1}
+                </button>
+              );
+            })}
+          </span>
+          <button
+            onClick={handleNextPage}
+            disabled={pageIndex + 1 === dataChunks.length}
+            className="btn datatable__next-page">
+            Next
+          </button>
+        </div>
       </div>
     </DataTable>
   );
@@ -225,15 +239,60 @@ const Table = ({ data = [], options }: TableProps) => {
 
 export default Table;
 
-const Row = (props: { data: data; dataKeys: dataKey[] }) => {
-  const { data, dataKeys } = props;
+const TableHeader = (props: {
+  title: string;
+  dataKey: string;
+  isSorted: boolean;
+  sortOrder: string;
+  setSortBy: React.Dispatch<React.SetStateAction<string>>;
+  setSortOrder: React.Dispatch<React.SetStateAction<'' | 'asc' | 'desc'>>;
+}) => {
+  const { title, dataKey, isSorted, sortOrder, setSortBy, setSortOrder } =
+    props;
+
+  const toggleSort = () => {
+    if (!isSorted) {
+      setSortOrder(() => 'asc');
+      setSortBy(() => dataKey);
+    } else
+      sortOrder === 'asc'
+        ? setSortOrder(() => 'desc')
+        : setSortOrder(() => 'asc');
+  };
 
   return (
-    <tr>
+    <th
+      className=""
+      data-issorted={isSorted}
+      data-sort={sortOrder}
+      data-key={dataKey}
+      onClick={toggleSort}>
+      {title}
+    </th>
+  );
+};
+
+const Row = (props: {
+  data: data;
+  dataKeys: dataKey[];
+  isEven: boolean;
+  sortBy: string;
+}) => {
+  const { data, dataKeys, isEven, sortBy } = props;
+
+  return (
+    <tr data-iseven={isEven}>
       {dataKeys.map((key, index) => {
-        if (key.position !== undefined) {
+        const { value, title, position } = key;
+        if (position !== undefined) {
           return (
-            <Cell key={`td-${key.value}-${index}`} entry={data[key.value]} />
+            <Cell
+              key={`td-${value}-${index}`}
+              keyValue={value}
+              entry={data[value]}
+              title={title}
+              isSorted={sortBy === value}
+            />
           );
         }
       })}
@@ -241,9 +300,18 @@ const Row = (props: { data: data; dataKeys: dataKey[] }) => {
   );
 };
 
-const Cell = (props: { entry: string | number }) => {
-  const { entry } = props;
-  return <td>{entry}</td>;
+const Cell = (props: {
+  entry: string | number;
+  title: string;
+  keyValue: string;
+  isSorted: boolean;
+}) => {
+  const { entry, title, keyValue, isSorted } = props;
+  return (
+    <td data-title={title} data-keyvalue={keyValue} data-issorted={isSorted}>
+      {entry}
+    </td>
+  );
 };
 
 const DataTable = styled.div`
@@ -258,19 +326,59 @@ const DataTable = styled.div`
   button {
     cursor: pointer;
   }
+
+  .datatable__header {
+    margin: 1rem 0;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .datatable__body {
+    width: 100%;
+    overflow-x: auto;
+    padding: 0 0 1rem 0;
+    &::-webkit-scrollbar {
+      height: 8px;
+    }
+    &::-webkit-scrollbar-track {
+      background-color: rgba(0, 0, 0, 0);
+    }
+    &::-webkit-scrollbar-thumb {
+      background-color: #ccc;
+      border-radius: 10px;
+    }
+  }
+
+  table {
+    border-collapse: collapse;
+    text-align: left;
+    width: 100%;
+  }
+
+  th,
+  td {
+    padding: 5px 10px;
+    white-space: nowrap;
+  }
+
+  [data-iseven='false'] {
+    background-color: #eeeeee;
+  }
+
+  td[data-issorted='true'] {
+    background-color: rgba(0,0,0,0.05);
+  }
+
+  .datatable__footer {
+    margin: 1rem 0;
+    display: flex;
+    justify-content: space-between;
+  }
+
   button.current {
     border-color: red;
     background-color: red;
     color: white;
     font-weight: 700;
-  }
-
-  .column-header {
-    display: flex;
-    align-items: center;
-  }
-  .sort-controller {
-    display: flex;
-    flex-direction: column;
   }
 `;
